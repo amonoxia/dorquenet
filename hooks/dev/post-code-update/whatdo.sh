bdf592a3da5f7bbda8632672a02a7b5a01ed33a8
Christia Hall <christia.hall@acquia.com>
2022-02-24 21:41:17 +0000

    More post code update hook testing

--- hooks/dev/post-code-update/whatdo.sh
+++ hooks/dev/post-code-update/whatdo.sh
@@ -8,5 +8,6 @@
 site="$1"
 target_env="$2"
 
-drush @$site.$target_env updb -y
-drush @$site.$target_env cim -y
+# drush @$site.$target_env updb -y
+# drush @$site.$target_env cim -y
+echo "Testing, testing. Hello world!"
